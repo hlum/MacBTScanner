f17bd4c92dc73dd8837c3a677adc9463c1f1a44c
aung <hlumaungphyo@gmail.com>
2025-03-23 21:39:03 +0900

    Initial Commit

---
+++ MacBTScanner/MacBTScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacBTScannerApp.swift
+//  MacBTScanner
+//
+//  Created by Hlwan Aung Phyo on 2025/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MacBTScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacBTScannerTests/MacBTScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacBTScannerTests.swift
+//  MacBTScannerTests
+//
+//  Created by Hlwan Aung Phyo on 2025/03/23.
+//
+
+import Testing
+@testable import MacBTScanner
+
+struct MacBTScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
