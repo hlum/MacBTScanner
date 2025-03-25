aec40fb53ccf153cc81d7dc6398c68be5f4d8c76
aung <hlumaungphyo@gmail.com>
2025-03-25 20:40:00 +0900

    bt manager completed

--- MacBTScanner/ContentView.swift
+++ MacBTScanner/ContentView.swift
@@ -6,15 +6,45 @@
 //
 
 import SwiftUI
+import CoreBluetooth
+
+final class ContentViewModel: ObservableObject {
+    let btManager = BluetoothManager.shared
+    
+    func toggleLED() {
+        guard let char = btManager.esp32Characteristic,
+        let data = "toggle".data(using: .utf8)
+        else {
+            Logger.standard.warning("Characteristic not found")
+            return
+        }
+        btManager.esp32?.writeValue(data, for: char, type: .withResponse)
+    }
+}
 
 struct ContentView: View {
+    @ObservedObject private var vm = ContentViewModel()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Circle()
+                .fill(vm.btManager.peripheralState.color)
+                .frame(maxHeight: 100)
+            
+            Button {
+                vm.toggleLED()
+            } label: {
+                VStack {
+                    Image(systemName: "sun.max")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                        .padding()
+                    
+                    Text("Toggle LED")
+                }
+            }
+
         }
+        .frame(minWidth: 400)
         .padding()
     }
 }
---
+++ MacBTScanner/PeripheralManager.swift
@@ -0,0 +1,42 @@
+//
+//  PeripheralManager.swift
+//  MacBTScanner
+//
+//  Created by Hlwan Aung Phyo on 2025/03/25.
+//
+
+
+class PeripheralManager: NSObject, CBPeripheralDelegate {
+    static let shared = PeripheralManager()
+    
+    private override init() { super.init() }
+    
+    var esp32: CBPeripheral?
+    
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: (any Error)?) {
+        for service in peripheral.services ?? [] {
+            peripheral.discoverCharacteristics([CBUUID(string: UUIDStrings.characteristicUUIDString)],
+                                               for: service)
+        }
+    }
+    
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: (any Error)?) {
+        for characteristic in service.characteristics ?? [] {
+            BluetoothManager.shared.esp32Characteristic = characteristic
+            if characteristic.uuid.uuidString == UUIDStrings.characteristicUUIDString {
+                esp32?.setNotifyValue(true, for: characteristic)
+            }
+        }
+    }
+    
+    
+    func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: (any Error)?) {
+        if let error {
+            Logger.standard.error("Error writing value: \(error)")
+        } else {
+            Logger.standard.info(">>> Sent data")
+        }
+    }
+}
